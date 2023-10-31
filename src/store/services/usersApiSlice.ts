e519fa3b1597b7addedfd262d3f0812a7f9fe390
Marko Trifunovic <trifunovicm@loni.ai>
2023-10-31 16:04:33 +0100

    adding env file

--- src/store/services/usersApiSlice.ts
+++ src/store/services/usersApiSlice.ts
@@ -1,12 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { UserType } from '../../screens/UsersScreen/model';
+import { APP_URL } from '@env';
+import { GET_USERS_PATH } from './helpers';
 
 export const usersApi = createApi({
   reducerPath: 'usersApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${APP_URL}` }),
   endpoints: builder => ({
     getUsers: builder.query({
-      query: () => '/users',
+      query: () => `${GET_USERS_PATH}`,
       transformResponse: (response: UserType[]) => response,
     }),
   }),
